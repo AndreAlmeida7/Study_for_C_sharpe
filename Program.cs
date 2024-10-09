33276720597a217f8b686629e39c468ece658735
AndreAlmeida7 <andre_ralmeida@hotmail.com>
2024-10-09 20:07:45 -0300

    Métodos com strings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
 Console.WriteLine("Hello, World!");
 
 // Para defirnir uma variável em C#, temos que especificar qual o tipo da variável primeiramente
@@ -8,4 +10,17 @@ Console.WriteLine("Hello, World!");
 
 string primeiro_nome = "Maria";
 string segundo_nome = "Antonieta";
-Console.WriteLine($"Meu amigos são {primeiro_nome} e {segundo_nome}");
+string terceiro_nome = "André Luiz Rodrigues";
+//Console.WriteLine($"Meu amigos são {primeiro_nome} e {segundo_nome}");
+
+// Aprendendo sobre a variável replace
+
+string friends = $"Meu amigos são {primeiro_nome} e {segundo_nome}";
+
+// Estou subtituindo a pavra antonieta por Luis
+Console.WriteLine(friends.Replace("Antonieta", "Luis"));
+
+// A variável "terceiro_nome" contém a string "André"? True or false?
+Console.WriteLine(terceiro_nome.Contains("André"));
+
+
--- obj/Debug/net8.0/Study_for_C_sharpe.AssemblyInfo.cs
+++ obj/Debug/net8.0/Study_for_C_sharpe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Study_for_C_sharpe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeb35f3eb5a11f1939e8f1eddecc1584f777d25e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+510cd8f7dfa12c2fa23016f6db3d7900be677597")]
 [assembly: System.Reflection.AssemblyProductAttribute("Study_for_C_sharpe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Study_for_C_sharpe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
