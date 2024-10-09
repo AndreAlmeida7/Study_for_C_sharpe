eeb35f3eb5a11f1939e8f1eddecc1584f777d25e
AndreAlmeida7 <andre_ralmeida@hotmail.com>
2024-10-09 19:31:37 -0300

    Comentários

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// Para defirnir uma variável em C#, temos que especificar qual o tipo da variável primeiramente
+// Strings são escritas com duas aspas
+//Todo final de linha no código precisa ter ponto e vírgula
+// o "f string" do python se escreve com o símbolo do cifrão
 String primeiro_nome = "Maria";
 String segundo_nome = "Antonieta";
 Console.WriteLine($"Meu amigos são {primeiro_nome} e {segundo_nome}");
