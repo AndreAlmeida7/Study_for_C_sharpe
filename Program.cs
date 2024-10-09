fb22ceb7cf0add44c610eb24257b5d014de5fa60
AndreAlmeida7 <andre_ralmeida@hotmail.com>
2024-10-09 19:29:17 -0300

    Entendendo sobre strings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+String primeiro_nome = "Maria";
+String segundo_nome = "Antonieta";
+Console.WriteLine($"Meu amigos são {primeiro_nome} e {segundo_nome}");
