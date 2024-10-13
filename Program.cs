2cb4768f94242640fd003c56b8d63448e84e6891
AndreAlmeida7 <andre_ralmeida@hotmail.com>
2024-10-13 10:50:32 -0300

    Alterações

--- Program.cs
+++ Program.cs
@@ -82,10 +82,10 @@ int sum_a_b = number_a + number_b;
 if (sum_a_b > 10 || number_a > 5) // || refere-se a lógica "ou", "or"
 { // É sempre recomendado colocar chaves entre o argumento de if e else. Significa ser mais explícito e direcional com seus comandos
     Console.WriteLine($"The answer {sum_a_b} is greater than 10"); 
-    Console.WriteLine("Ou number a is number 5"); 
+    Console.WriteLine("Or number a is number 5"); 
 } 
 else
 {
     Console.WriteLine($"The answer {sum_a_b} is less than 10");
-    Console.WriteLine("Or the number a is not number 5"); 
+    Console.WriteLine("And the number a is not number 5"); 
 }
