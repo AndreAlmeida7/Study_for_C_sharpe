e7b5ee19064b33b060aec0200deb8614424d6888
AndreAlmeida7 <andre_ralmeida@hotmail.com>
2024-10-13 15:50:59 -0300

    Podemos adicionar, perguntar se contém e deletar coisas em listas!

--- list-t-colections-of-data/Program.cs
+++ list-t-colections-of-data/Program.cs
@@ -1,5 +1,9 @@
 ﻿var names = new List<string> {"André", "Ana", "Felipe"}; // Declarando a variável "names" como uma lista de variáveis que receberá os nomes citados.
 
+names.Add("David");
+names.Add("Maria");
+names.Add("kaka");
+
 foreach (var name in names) // Para cada nome dentro da lista de nomes faça o seguinte:
 {
     Console.WriteLine($"Hello {name.ToUpper()}!");  // imprima "Hello (upper name of person)!"  
